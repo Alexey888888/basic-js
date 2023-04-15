6355011c5e87ba8410ca70a73f5059cbb3735cc2
Alexey888888 <brdkmail@gmail.com>
2023-04-15 14:07:29 +0300

    fix part extended-reapeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,11 +16,12 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 function repeater(str, options) {
+  //str = str.toString();
   let repeatTimes;
-  let separator;
+  let separator = "+";
   let addition;
   let additionRepeatTimes;
-  let additionSeparator;
+  let additionSeparator = "|";
   let res = "";
   for (let prop in options) {
     if (prop === "repeatTimes") repeatTimes = options[prop];
@@ -31,21 +32,21 @@ function repeater(str, options) {
   }
   if (repeatTimes) {
     if (additionRepeatTimes) {
-      for (let i = 0; i < repeatTimes - 1; i++) {
+      for (let i = 1; i < repeatTimes; i++) {
         res += str;
-        for (let j = 0; j < additionRepeatTimes - 1; j++) {
+        for (let j = 1; j < additionRepeatTimes; j++) {
           res += addition + additionSeparator;
         }
         res += addition;
         res += separator;
       }
       res += str;
-      for (let ii = 0; ii < additionRepeatTimes - 1; ii++) {
+      for (let ii = 1; ii < additionRepeatTimes; ii++) {
         res += addition + additionSeparator;
       }
       res += addition;
     } else {
-      for (let jj = 0; jj < repeatTimes - 1; jj++) {
+      for (let jj = 1; jj < repeatTimes; jj++) {
         res += str + addition + separator;
       }
       res += str + addition;
@@ -53,7 +54,7 @@ function repeater(str, options) {
   } else {
     if (additionRepeatTimes) {
       res += str;
-      for (let iii = 0; iii < additionRepeatTimes - 1; iii++) {
+      for (let iii = 1; iii < additionRepeatTimes; iii++) {
         res += addition + additionSeparator;
       }
       res += addition;
